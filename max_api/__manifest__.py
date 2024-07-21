8773f98a7c5efe0a6d853f8355ea941fcef5b9c9
donhat-dev <donhat.hn@gmail.com>
2024-07-22 01:19:57 +0700

    []

---
+++ max_api/__manifest__.py
@@ -0,0 +1,13 @@
+{
+        'name': 'Max API',
+        'description': 'Execute postgreSQL query into Odoo interface',
+        'author': 'Yvan Dotet',
+        'depends': ['base'],
+        'application': True,
+        'version': '12.0.1.0.3',
+        'installable': True,
+
+        'data': [
+            ],
+}
+
